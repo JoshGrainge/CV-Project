f3699232f24125b228dfc884cf8b622fd5a00289
Josh Grainge <josh.grainge@hotmail.com>
2023-02-23 18:34:20 -0500

    Incorporated styling into layout prototype

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   constructor(props) {
@@ -9,55 +10,68 @@ class App extends Component {
   render() {
     return (
       <div className="resume-container">
-        <h1>Josh Grainge</h1>
-        <h3>Location | Phone Num | Email</h3>
+        <div className="header">
+          <h1>Josh Grainge</h1>
+          <p>Location</p>
+          <p>Phone</p>
+          <p>Email</p>
+        </div>
 
-        <div>
+        <div className="resume-body">
           <h2>Education</h2>
           <hr />
-          <div className="columns">
-            <p>School name</p>
-            <p>Location</p>
-          </div>
-          <div className="columns">
-            <p>Diploma name</p>
-            <p>Start - End</p>
+          <div className="text-section">
+            <div className="text-row">
+              <p>School name</p>
+              <p>Location</p>
+            </div>
+            <div className="text-row">
+              <p>Diploma name</p>
+              <p>Start - End</p>
+            </div>
           </div>
 
           <h2>Experience</h2>
           <hr />
-          <div className="columns">
-            <p>Title</p>
-            <p>Start - End</p>
+          <div className="text-section">
+            <div className="text-row">
+              <p>Title</p>
+              <p>Start - End</p>
+            </div>
+            <div className="text-row">
+              <p>Company</p>
+              <p>Location</p>
+            </div>
+            <ul>
+              <li>Description</li>
+              <li>Description</li>
+              <li>Description</li>
+            </ul>
           </div>
-          <div className="columns">
-            <p>Company</p>
-            <p>Location</p>
-          </div>
-          <ul>
-            <li>Description</li>
-            <li>Description</li>
-            <li>Description</li>
-          </ul>
 
           <h2>Projects</h2>
           <hr />
-          <div className="columns">
-            <p>Title</p>
-            <p> | JavaScript CSS HTML</p>
-            <p>Start - End</p>
+          <div className="text-section">
+            <div className="text-row">
+              <p>Title | JavaScript CSS HTML</p>
+              <p>Start - End</p>
+            </div>
+            <ul>
+              <li>Description</li>
+              <li>Description</li>
+              <li>Description</li>
+            </ul>
           </div>
-          <ul>
-            <li>Description</li>
-            <li>Description</li>
-            <li>Description</li>
-          </ul>
 
           <h2>Technical Skills</h2>
           <hr />
-          <p>Languages: JavaScript, CSS, HTML, C#, Python, MySQL</p>
-          <p>Frameworks: React</p>
-          <p>Tools: Git, Visual Studio, ItelliJ, Github</p>
+          <div className="text-section">
+            <div className="skills-text">
+              <p>Languages: JavaScript, CSS, HTML, C#, Python, MySQL</p>
+              <p>Frameworks: React</p>
+              <p>Tools: Git, Visual Studio, ItelliJ, Github</p>
+            </div>
+          </div>
         </div>
       </div>
     );
