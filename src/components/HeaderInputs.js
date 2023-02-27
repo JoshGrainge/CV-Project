6ffe8b04233ab6877ae192491068d8ba1d710b1b
Josh Grainge <josh.grainge@hotmail.com>
2023-02-27 11:38:55 -0500

    Added generic header template information

--- src/components/HeaderInputs.js
+++ src/components/HeaderInputs.js
@@ -5,7 +5,16 @@ class HeaderInputs extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      name: "Full Name",
+      location: "Location",
+      phone: "555-5555",
+      email: "Email@email.com",
+    };
+
     this.onChange = this.onChange.bind(this);
+
+    this.submitChange();
   }
 
   onChange(e) {
@@ -34,13 +43,33 @@ class HeaderInputs extends Component {
         <h3>Personal Info:</h3>
         <div className="inputs-container">
           <label>Name:</label>
-          <input type={"text"} id={"name"} onChange={this.onChange}></input>
+          <input
+            type={"text"}
+            id={"name"}
+            value={this.state.name}
+            onChange={this.onChange}
+          ></input>
           <label>Address:</label>
-          <input type={"text"} id={"location"} onChange={this.onChange}></input>
+          <input
+            type={"text"}
+            id={"location"}
+            value={this.state.location}
+            onChange={this.onChange}
+          ></input>
           <label>Phone:</label>
-          <input type={"text"} id={"phone"} onChange={this.onChange}></input>
+          <input
+            type={"text"}
+            id={"phone"}
+            value={this.state.phone}
+            onChange={this.onChange}
+          ></input>
           <label>Email:</label>
-          <input type={"text"} id={"email"} onChange={this.onChange}></input>
+          <input
+            type={"text"}
+            id={"email"}
+            value={this.state.email}
+            onChange={this.onChange}
+          ></input>
         </div>
       </div>
     );
