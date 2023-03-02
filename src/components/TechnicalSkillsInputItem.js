8bb0e5f796d6fa9d4b0d299bfd5fdce1ff75ae9e
Josh Grainge <josh.grainge@hotmail.com>
2023-03-02 13:09:36 -0500

    Altered TechnicalSkills components to be function components

--- src/components/TechnicalSkillsInputItem.js
+++ src/components/TechnicalSkillsInputItem.js
@@ -1,68 +1,36 @@
-import React, { Component } from "react";
-import TechnicalSkillInfo from "../TechnicalSkillInfo";
+import React, { useState, useEffect } from "react";
 
-class TechnicalSkillsInputItem extends Component {
-  constructor(props) {
-    super(props);
-    this.props = props;
+function TechnicalSkillsInputItem(props) {
+  const [title, setTitle] = useState(props.info.title);
+  const [text, setText] = useState(props.info.text);
 
-    this.state = {
-      title: this.props.info.title,
-      text: this.props.info.text,
-    };
+  const remove = () => {
+    props.deleteInput(props.index);
+  };
 
-    this.handleClick = this.handleClick.bind(this);
-    this.onChange = this.onChange.bind(this);
-    this.submitChange = this.submitChange.bind(this);
-  }
-
-  onChange(e) {
-    this.setState(
-      {
-        [e.target.id]: e.target.value,
-      },
-      this.submitChange
-    );
-  }
-
-  submitChange() {
-    this.props.onChange(
-      this.props.index,
-      TechnicalSkillInfo(this.state.title, this.state.text)
-    );
-  }
-
-  handleClick() {
-    this.setState({
-      title: "",
-      text: "",
+  useEffect(() => {
+    props.onChange(props.index, {
+      title: title,
+      text: text,
     });
-    this.props.deleteInput(this.props.index);
-  }
+  }, [title, text]);
 
-  render() {
-    return (
-      <div className="inputs-container">
-        <label>Title:</label>
-        <input
-          type={"text"}
-          id="title"
-          value={this.props.info.title}
-          onChange={this.onChange}
-        />
-        <label>Text:</label>
-        <input
-          type={"text"}
-          id="text"
-          value={this.props.info.text}
-          onChange={this.onChange}
-        />
-        <button className="mid-length-button" onClick={this.handleClick}>
-          Remove
-        </button>
-      </div>
-    );
-  }
+  useEffect(() => {
+    setTitle(props.info.title);
+    setText(props.info.text);
+  }, [props.info]);
+
+  return (
+    <div className="inputs-container">
+      <label>Title:</label>
+      <input value={title} onChange={(e) => setTitle(e.target.value)} />
+      <label>Text:</label>
+      <input value={text} onChange={(e) => setText(e.target.value)} />
+      <button className="mid-length-button" onClick={remove}>
+        Remove
+      </button>
+    </div>
+  );
 }
 
 export default TechnicalSkillsInputItem;
--- src/components/TechnicalSkillsInputs.js
+++ src/components/TechnicalSkillsInputs.js
@@ -1,8 +1,71 @@
-import React, { Component } from "react";
+import React, { useState, useEffect, Component } from "react";
 import TechnicalSkillsInputItem from "./TechnicalSkillsInputItem";
 import TechnicalSkillInfo from "../TechnicalSkillInfo";
 
-class TechnicalSkillsInputs extends Component {
+let inputs = [];
+
+const defaultTechnicalSkillData = TechnicalSkillInfo(
+  "Technical skill title",
+  "Technical skill description"
+);
+
+function TechnicalSkillsInputs(props) {
+  const [technicalSkillInputs, setTechnicalSkillInputs] = useState([
+    defaultTechnicalSkillData,
+  ]);
+
+  useEffect(() => {
+    props.onChange(technicalSkillInputs);
+
+    // Reassign education inputs
+    inputs = [];
+    for (let i = 0; i < technicalSkillInputs.length; i++) {
+      inputs.push(
+        <TechnicalSkillsInputItem
+          info={technicalSkillInputs[i]}
+          key={i}
+          index={i}
+          deleteInput={removeInput}
+          onChange={changeInput}
+        />
+      );
+    }
+  }, [technicalSkillInputs]);
+
+  const removeInput = (index) => {
+    setTechnicalSkillInputs([
+      ...technicalSkillInputs.slice(0, index),
+      ...technicalSkillInputs.slice(index + 1),
+    ]);
+  };
+
+  const changeInput = (index, newInput) => {
+    setTechnicalSkillInputs([
+      ...technicalSkillInputs.slice(0, index),
+      newInput,
+      ...technicalSkillInputs.slice(index + 1),
+    ]);
+  };
+
+  const addInput = () => {
+    setTechnicalSkillInputs([
+      ...technicalSkillInputs,
+      defaultTechnicalSkillData,
+    ]);
+  };
+
+  return (
+    <div id="technical-skills-inputs" className="section-inputs">
+      <h3>Technical Skills:</h3>
+      <div>{inputs}</div>
+      <button className="mid-length-button" onClick={addInput}>
+        Add
+      </button>
+    </div>
+  );
+}
+
+class TechnicalSkillsInputsClass extends Component {
   constructor(props) {
     super(props);
     this.props = props;
