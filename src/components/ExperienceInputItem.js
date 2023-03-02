ea24343b2aeca17cf0a254a7cd7d5a5dc5bb924d
Josh Grainge <josh.grainge@hotmail.com>
2023-03-02 12:29:29 -0500

    Changed to function component

--- src/components/ExperienceInputItem.js
+++ src/components/ExperienceInputItem.js
@@ -1,114 +1,65 @@
-import React, { Component } from "react";
-import ExperienceInfo from "../ExperienceInfo";
+import React, { useState, useEffect, Component } from "react";
 import DescriptionInputs from "./DescriptionInputs";
 
-class ExperienceInputItem extends Component {
-  constructor(props) {
-    super(props);
+function ExperienceInputItem(props) {
+  const [title, setTitle] = useState(props.info.title);
+  const [start, setStart] = useState(props.info.start);
+  const [end, setEnd] = useState(props.info.end);
+  const [company, setCompany] = useState(props.info.company);
+  const [location, setLocation] = useState(props.info.location);
+  const [descriptions, setDescriptions] = useState(props.info.descriptions);
 
-    this.state = {
-      title: this.props.info.title,
-      start: this.props.info.start,
-      end: this.props.info.end,
-      company: this.props.info.company,
-      location: this.props.info.location,
-      descriptions: this.props.info.descriptions,
-    };
+  const remove = () => {
+    props.deleteInput(props.index);
+  };
 
-    this.handleClick = this.handleClick.bind(this);
-    this.onChange = this.onChange.bind(this);
-    this.submitChange = this.submitChange.bind(this);
-  }
-
-  onChange(e) {
-    this.setState(
-      {
-        [e.target.id]: e.target.value,
-      },
-      this.submitChange
-    );
-  }
-
-  submitChange() {
-    this.props.onChange(
-      this.props.index,
-      ExperienceInfo(
-        this.state.title,
-        this.state.start,
-        this.state.end,
-        this.state.company,
-        this.state.location,
-        this.state.descriptions
-      )
-    );
-  }
-
-  handleClick() {
-    this.setState({
-      title: "",
-      start: "",
-      end: "",
-      company: "",
-      location: "",
-      descriptions: [],
+  useEffect(() => {
+    props.onChange(props.index, {
+      title: title,
+      start: start,
+      end: end,
+      company: company,
+      location: location,
+      descriptions: descriptions,
     });
-    this.props.deleteInput(this.props.index);
-  }
+  }, [title, start, end, company, location, descriptions]);
 
-  render() {
-    return (
-      <div className="inputs-container">
-        <label>Title:</label>
-        <input
-          type={"text"}
-          id="title"
-          value={this.props.info.title}
-          onChange={this.onChange}
-        />
-        <label>Start:</label>
-        <input
-          type={"text"}
-          id="start"
-          value={this.props.info.start}
-          onChange={this.onChange}
-        />
-        <label>End:</label>
-        <input
-          type={"text"}
-          id="end"
-          value={this.props.info.end}
-          onChange={this.onChange}
-        />
-        <label>Company:</label>
-        <input
-          type={"text"}
-          id="company"
-          value={this.props.info.company}
-          onChange={this.onChange}
-        />
-        <label>Location:</label>
-        <input
-          type={"text"}
-          id="location"
-          value={this.props.info.location}
-          onChange={this.onChange}
-        />
-        <div>
-          <label>Descriptions:</label>
-          <div className="description-container">
-            <DescriptionInputs
-              id="descriptions"
-              descriptions={this.props.info.descriptions}
-              onChange={this.onChange}
-            />
-          </div>
+  useEffect(() => {
+    setTitle(props.info.title);
+    setStart(props.info.start);
+    setEnd(props.info.end);
+    setCompany(props.info.company);
+    setLocation(props.info.location);
+    setDescriptions(props.info.descriptions);
+  }, [props.info]);
+
+  return (
+    <div className="inputs-container">
+      <label>Title:</label>
+      <input value={title} onChange={(e) => setTitle(e.target.value)} />
+      <label>Start:</label>
+      <input value={start} onChange={(e) => setStart(e.target.value)} />
+      <label>End:</label>
+      <input value={end} onChange={(e) => setEnd(e.target.value)} />
+      <label>Company:</label>
+      <input value={company} onChange={(e) => setCompany(e.target.value)} />
+      <label>Location:</label>
+      <input value={location} onChange={(e) => setLocation(e.target.value)} />
+      <div>
+        <label>Descriptions:</label>
+        <div className="description-container">
+          <DescriptionInputs
+            id="descriptions"
+            descriptions={descriptions}
+            onChange={setDescriptions}
+          />
         </div>
-        <button className="mid-length-button" onClick={this.handleClick}>
-          Remove
-        </button>
       </div>
-    );
-  }
+      <button className="mid-length-button" onClick={remove}>
+        Remove
+      </button>
+    </div>
+  );
 }
 
 export default ExperienceInputItem;
